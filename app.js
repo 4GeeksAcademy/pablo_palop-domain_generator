b6114ea0bb4e14f258416e985ee6f491f229c5cc
pablopalop <130085149+pablopalop@users.noreply.github.com>
2025-02-21 18:32:06 +0000

    First Commit

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = () => {
-  //write your code here
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = domainGenerator();
-  })
-};
-
-
 let pronoun = ['the', 'your']
 let adj = ['great', 'big', 'awesome']
 let color = ['red', 'blue', 'yellow']
 let object = ['door', 'window', 'table']
 
 
-const domainGenerator = (param1,param2) => {
+const domainGenerator = () => {
     pronoun.forEach(item1 => {
         adj.forEach(item2 => {
             color.forEach(item3 => {
